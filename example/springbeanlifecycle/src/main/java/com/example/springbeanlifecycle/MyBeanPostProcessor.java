e259a918b31955b9bfd140603995b5f047a64092
teahel <li.teahel@gmail.com>
2021-09-27 21:00:17 +0800

    周期

--- example/springbeanlifecycle/src/main/java/com/example/springbeanlifecycle/MyBeanPostProcessor.java
+++ example/springbeanlifecycle/src/main/java/com/example/springbeanlifecycle/MyBeanPostProcessor.java
@@ -2,18 +2,19 @@ package com.example.springbeanlifecycle;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyBeanPostProcessor implements BeanPostProcessor {
+public class MyBeanPostProcessor implements DestructionAwareBeanPostProcessor  {
 
     //在初始化之前工作
     @Nullable
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof User) {
-            System.out.println("postProcessBeforeInitialization:"+bean+"|"+beanName);
+            System.out.println("postProcessBeforeInitialization: "+bean+" -"+beanName);
         }
         return bean;
     }
@@ -21,8 +22,16 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
     //在初始化之后工作
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof User) {
-            System.out.println("postProcessAfterInitialization:"+bean+"|"+beanName);
+            System.out.println("postProcessAfterInitialization: "+bean+" -"+beanName);
         }
         return bean;
+    }
+
+    //在销毁之前将此BeanPostProcessor 应用于给定的bean实例。能够调用自定义回调
+    @Override
+    public void postProcessBeforeDestruction(Object bean, String beanName) throws BeansException {
+        if (bean instanceof User) {
+            System.out.println("postProcessBeforeDestruction: "+bean+" -"+beanName);
+        }
     }
 }
--- example/springbeanlifecycle/src/main/java/com/example/springbeanlifecycle/User.java
+++ example/springbeanlifecycle/src/main/java/com/example/springbeanlifecycle/User.java
@@ -25,12 +25,13 @@ import javax.servlet.ServletContext;
 @Configuration
 public class User implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAware,
         EnvironmentAware, EmbeddedValueResolverAware, ResourceLoaderAware, ApplicationEventPublisherAware, MessageSourceAware,
-        ApplicationContextAware, ServletContextAware, InitializingBean , DestructionAwareBeanPostProcessor, DisposableBean{
+        ApplicationContextAware, ServletContextAware, InitializingBean , DisposableBean{
 
+    // 初始化方法
     public void initMethod(){
         System.out.println("initMethod ....");
     }
-
+    // 销毁方法
     public void destroyMethod() {
         System.out.println("destroyMethod ....");
     }
@@ -41,43 +42,52 @@ public class User implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAwa
 
     @PostConstruct
     public void myPostConstruct() {
-        System.out.println("PostConstruct ....");
+        System.out.println("PostConstruct注解作用在方法上，在依赖注入完成后进行一些初始化操作。 ....");
     }
 
     // @PreDestroy：在容器销毁bean之前通知我们进行清理工作
     @PreDestroy
     public void myDestroy() {
-        System.out.println("myDestroy ....");
+        System.out.println("@PreDestroy：在容器销毁bean之前通知我们进行清理工作 ....");
     }
 
+    // 将 bean 类加载器提供给 bean 实例的回调。(在普通属性设置之后调用，在InitializinngBean.afterPropertiesSet()方法之前调用)
     @Override
     public void setBeanClassLoader(ClassLoader classLoader) {
         System.out.println("classLoader.....");
     }
 
+    // 传入Bean工厂的实例 (在普通属性设置之后调用，在InitializinngBean.afterPropertiesSet()方法之前调用)
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("beanFactory.....");
-
     }
 
+    // 在创建此 bean 的 bean 工厂中设置 bean 的名称。(在普通属性设置之后调用，在InitializinngBean.afterPropertiesSet()方法之前调用)
     @Override
     public void setBeanName(String name) {
         System.out.println("beanName.....");
 
     }
 
+    //在设置所有 bean 属性之后由包含 BeanFactory 调用
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("afterPropertiesSet.....");
 
     }
 
+    //设置此对象运行所在的 ApplicationContext。通常此调用将用于初始化对象。(在普通属性设置之后调用，在InitializinngBean.afterPropertiesSet()方法之前调用)
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("applicationContext.....");
 
     }
+
+    //设置运行此对象的 ApplicationEventPublisher。
+    //在填充普通 bean 属性之后但在初始化回调（如 InitializingBean 的 afterPropertiesSet 或自定义初始化方法）之前调用。
+    // 在 ApplicationContextAware 的 setApplicationContext 之前调用。
+    // 使用事件发布者发布消息: 可以通过 ApplicationEventPublisher 的 publishEvent() 方法发布消息。
 
     @Override
     public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
@@ -85,18 +95,21 @@ public class User implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAwa
 
     }
 
+    //设置 StringValueResolver 以用于解析嵌入的定义值。
     @Override
     public void setEmbeddedValueResolver(StringValueResolver resolver) {
         System.out.println("resolver.....");
-
     }
 
+    //设置该实例运行的环境。(我们运行时有开发环境dev,生产环境 pro,测试环境 test，如：application-test.yml为测试环境专有的配置文件)
     @Override
     public void setEnvironment(Environment environment) {
         System.out.println("environment.....");
-
     }
 
+    // 设置此对象运行所在的 MessageSource（消息源）。
+    //在填充普通 bean 属性之后但在初始化回调（如 InitializingBean 的 afterPropertiesSet 或自定义初始化方法）之前调用。
+    // 在 ApplicationContextAware 的 setApplicationContext 之前调用。
     @Override
     public void setMessageSource(MessageSource messageSource) {
         System.out.println("messageSource.....");
@@ -107,36 +120,27 @@ public class User implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAwa
         System.out.println("litianjun!");
     }
 
+    // 设置运行此对象的资源加载器。
+    //在填充普通 bean 属性之后但在初始化回调（如 InitializingBean 的 afterPropertiesSet 或自定义初始化方法）之前调用。
+    // 在 ApplicationContextAware 的 setApplicationContext 之前调用。
     @Override
     public void setResourceLoader(ResourceLoader resourceLoader) {
         System.out.println("resourceLoader.....");
     }
 
+    // 在web application情况下赋值
+    //在填充普通 bean 属性之后但在初始化回调（如 InitializingBean 的 afterPropertiesSet 或自定义初始化方法）之前调用。
+    // 在 ApplicationContextAware 的 setApplicationContext 之后调用。
     @Override
     public void setServletContext(ServletContext servletContext) {
         System.out.println("servletContext.....");
-
     }
 
 
-    //在销毁 bean 时由包含 BeanFactory 调用。
+    //由包含此bean的 BeanFactory 调用去销毁该bean。
     @Override
     public void destroy() throws Exception {
-        System.out.println("destroy.......");
+        System.out.println("由包含此bean的 BeanFactory 调用去销毁该bean.......");
     }
 
-    @Override
-    public void postProcessBeforeDestruction(Object bean, String beanName) throws BeansException {
-        System.out.println("postProcessBeforeDestruction.....");
-    }
-
-    @Override
-    public boolean requiresDestruction(Object bean) {
-        return DestructionAwareBeanPostProcessor.super.requiresDestruction(bean);
-    }
-
- /*   @Bean
-    public MyBeanPostProcessor getBeanPostProcessor(){
-        return new MyBeanPostProcessor();
-    }*/
-}
+ }
