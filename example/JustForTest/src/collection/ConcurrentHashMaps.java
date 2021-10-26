389782dc298edaf53489ed6ba4ca5811ceafd7c3
teahel <li.teahel@gmail.com>
2021-10-27 00:30:46 +0800

    zookeeper

---
+++ example/JustForTest/src/collection/ConcurrentHashMaps.java
@@ -0,0 +1,10 @@
+package collection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMaps {
+    public static void main(String[] args) {
+        ConcurrentHashMap concurrentHashMap  = new ConcurrentHashMap();
+
+    }
+}
---
+++ example/greeting/src/main/java/com/example/greeting/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.greeting;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private HelloWorldClient helloWorldClient;
+
+    @GetMapping("/get-greeting")
+    public String greeting() {
+        return helloWorldClient.helloWorld();
+    }
+
+}
